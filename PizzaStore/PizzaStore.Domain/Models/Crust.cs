fd670a8765212caf20693ba4af413492e9f6eba3
jordan pittman <jpittman306@gmail.com>
2020-07-21 13:34:46 -0500

    size/crust update

--- PizzaStore/PizzaStore.Domain/Models/Crust.cs
+++ PizzaStore/PizzaStore.Domain/Models/Crust.cs
@@ -2,5 +2,30 @@ namespace PizzaStore.Domain.Models
 {
     public class Crust
     {
+        public string _crust(string c)
+        {
+            int choice;
+            int.TryParse(c, out choice);
+
+            if(choice == 1)
+            {
+                var crust = "regular";
+                return crust;
+            }
+            else if(choice == 2)
+            {
+                var crust = "stuffed";
+                return crust;
+            }
+            else if(choice == 3)
+            {
+                var crust = "Thin";
+                return crust;
+            }
+            else
+            {
+                return "Please make a valid selection";
+            }
+        }
     }
 }
--- PizzaStore/PizzaStore.Domain/Models/Size.cs
+++ PizzaStore/PizzaStore.Domain/Models/Size.cs
@@ -2,5 +2,30 @@ namespace PizzaStore.Domain.Models
 {
     public class Size
     {
+        public string _size(string s)
+        {
+            int choice;
+            int.TryParse(s, out choice);
+
+            if (choice == 1)
+            {
+                var size = "small";
+                return size;
+            }
+            else if (choice == 2)
+            {
+                var size = "medium";
+                return size;
+            }
+            else if (choice == 3)
+            {
+                var size = "large";
+                return size;
+            }
+            else
+            {
+                return "Please make a valid selection";
+            }
+        }
     }
 }
